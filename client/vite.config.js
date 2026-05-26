79a61d325644b00c400d516997b5d85dc42f1d35
Guillaume Zufferey <guillaume.zufferey@gmail.com>
2026-05-26 09:32:23 +0200

    fix github pages base path

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,15 @@
-import { configDefaults, defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
+import { configDefaults, defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/graphql-todo/",
   server: {
     port: 5173,
   },
   test: {
-    environment: 'jsdom',
-    exclude: [...configDefaults.exclude, 'e2e/**'],
-    setupFiles: './src/test/setup.js',
+    environment: "jsdom",
+    exclude: [...configDefaults.exclude, "e2e/**"],
+    setupFiles: "./src/test/setup.js",
   },
 });
